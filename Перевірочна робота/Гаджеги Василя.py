4abc5ed557139f7e995c1ce6aef9dad265767eb5
Vasa Hadzheha <hadzheha.vasyl@student.uzhnu.edu.ua>
2020-12-31 13:00:27 +0200

    Update Гаджеги Василя.py

--- Перевірочна робота/Гаджеги Василя.py
+++ Перевірочна робота/Гаджеги Василя.py
@@ -2,24 +2,32 @@
 class Something:
 
     def result(self):
-        x=[]
-        y=[]
+        x = []
+        y = []
         with open("Points.txt") as f:
-            for num, row in enumerate(f,1):  # row= '313 4 52'
+            for num, row in enumerate(f, 1):  # row= '313 4 52'
                 numbers = list(map(float, row.split(' ')))  # ['313','4','52']
-                if num==1:
+                if num == 1:
                     for el in numbers:
-                        if el <0:
+                        if el < 0:
                             x.append(el)
-                elif num==2:
+                elif num == 2:
                     for el in numbers:
-                        if el>0:
+                        if el > 0:
                             y.append(el)
 
-        return print("x={0}'\n'y={1}".format(x,y))
+        print("x={0}'\n'y={1}".format(x, y))
+
+        if len(x)>len(y):
+            for i in range(len(y)):
+                print("Точка-{0} =({1},{2})".format(i+1,x[i],y[i]))
+
+        else:
+            for i in range(len(x)):
+                print("Точка-{0} =({1},{2})".format(i+1,x[i],y[i]))
 
 
-r=Something()
+r = Something()
 r.result()
 
 
