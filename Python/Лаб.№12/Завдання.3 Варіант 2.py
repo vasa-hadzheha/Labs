e48fed4befd0ba26051b451ffbf3a62b5a91c642
Vasa Hadzheha <hadzheha.vasyl@student.uzhnu.edu.ua>
2021-02-13 18:34:30 +0200

    new

--- Python/Лаб.№12/Завдання.3 Варіант 2.py
+++ Python/Лаб.№12/Завдання.3 Варіант 2.py
@@ -11,17 +11,17 @@ class Matrix:
     #     m=[[float(input("m[{0}][{1}]=".format(i,j))) for j in range(self.count_column)] for i in range(self.count_row)]
     #     return m
     def show_matrix(self):
-        b = []
-        for row in self.matrix():
-            row_str = ['{0:5d}'.format(el) for el in row]
-            b.append(row_str)
-
-        b = [''.join(['{0:7d}'.format(el) for el in row]) for row in  self.matrix()]
-        print("Ваша матриця:",*b, sep='\n')
-
+        # b = []
+        # for row in self.matrix:
+        #     row_str = ['{0:5d}'.format(el) for el in row]
+        #     b.append(row_str)
+        #
+        # b = [''.join(['{0:7d}'.format(el) for el in row]) for row in  self.matrix()]
+        # print("Ваша матриця:",*b, sep='\n')
+        return print(self.matrix)
 
     def __getitem__(self,el):
-        for i in range(self.matrix):            #Не читає значення m--------------------------------------- (Вказувати значення у функції?)
+        for i in range(self.count_row):
             if el in self.matrix[i]==True:
                 return el
             else:
@@ -29,12 +29,12 @@ class Matrix:
 
     def __setitem__(self,row_count,column_count, value):
         if row_count in list(self.count_row)==True and column_count in list(self.count_column)==True:
-            self.matrix[column_count][row_count]=value
+            self.matrix[row_count][column_count]=value
         else:
             raise Exception("Значення введені неправильно!!!")    #Error--не працює
 
     def __delitem__(self,el):
-        for i in range(self.matrix):
+        for i in range(self.count_row):
             if el in self.matrix[i]==True:
                 self.matrix[i].remove(el)
             else:
@@ -43,25 +43,26 @@ class Matrix:
 
     def max(self):
         h=[]
-        for i in range(self.matrix):
+        for i in range(len(self.matrix)):
             l=max(self.matrix[i])
-            h+=l
-        return max(h)
+            h+=[l]
+        return print(max(h))
 
     def min(self):
         h = []
-        for i in range(self.matrix):
+        for i in range(len(self.matrix)):
             l = min(self.matrix[i])
-            h += l
-        return min(h)
+            h += [l]
+        return print(min(h))
 
 
 m=[[float(input("m[{0}][{1}]=".format(i,j))) for j in range(3)] for i in range(3)]
 d=Matrix(3,3,m)
 d.show_matrix()
-# d.min()
-# d.max
-# d[5]
-# d[2,1,1000]
-# del d[1]
+d.min()
+d.max()
+# d[100]()
+# d[2,1,1000]()
+del d[1]
 
+d.show_matrix()
