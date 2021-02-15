a3572c6203c07e28cdc61055592157f21c2db5d3
Vasa Hadzheha <hadzheha.vasyl@student.uzhnu.edu.ua>
2021-02-15 23:06:22 +0200

    C#(lab)

---
+++ C#/Лаб.№5/Проба/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проба
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10231;
+            int b = 10;
+            double c = a % b;
+            Console.WriteLine(c);
+        }
+    }
+}
