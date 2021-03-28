27ecae8ccea9d24740f90f231cacf0474a0b4db6
Vasa Hadzheha <hadzheha.vasyl@student.uzhnu.edu.ua>
2021-03-28 20:29:01 +0300

    new

---
+++ C#/Лаб.№10/Завд.1 Вар 8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Завд._1_Вар_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StraightInFlatness e = new StraightInFlatness(2);
+            StraightInFlatness j = new StraightInFlatness(3);
+            //j.input();
+            e.input();
+            e.print();
+            //e.parallelism(j);
+            e.belonging(5, -1);
+
+        }
+    }
+}
---
+++ C#/Лаб.№10/Завд.2 Вар 8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Завд._2_Вар_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Monitor m = new Monitor("Asus", 2016, 2019, 5000, 20, 19);
+            m.age(2021);
+            m.enableDisp(20,19);
+            m.koef(20,19);
+        }
+    }
+}
