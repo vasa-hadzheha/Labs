21fac079a956e5907b4adcd3e4e34ec0c27eb77e
vasa-hadzheha <73876508+vasa-hadzheha@users.noreply.github.com>
2021-10-19 11:57:18 +0300

    new

--- HTML CSS JavaScript/Lab3/lab3.2_js/declar_var2.js
+++ HTML CSS JavaScript/Lab3/lab3.2_js/declar_var2.js
@@ -2,7 +2,7 @@ var images = ["https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid
 "http://pm1.narvii.com/6722/1224d143b8d165025dc539dd3b3e92bb865d5ba6_00.jpg",
 "https://static.wikia.nocookie.net/runescape2/images/5/56/Frog_%28NPC%29.png/revision/latest?cb=20160531202106",
 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHDOksrwN7pDuGkNIgqdqVU4okmutHU4P9DA&usqp=CAU"]
-function ranImage(arrImg){
+function ranImage(){
     var container = document.getElementById('appendImage');
     var img = document.createElement('img');
     img.src = images[Math.floor(Math.random()*4)];
