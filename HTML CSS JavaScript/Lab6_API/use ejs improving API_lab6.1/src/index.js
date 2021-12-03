d63cdff15ff76e92f89695a05a0921e8058e68ec
vasa-hadzheha <73876508+vasa-hadzheha@users.noreply.github.com>
2021-12-03 13:54:13 +0200

    new

--- HTML CSS JavaScript/Lab6_API/use ejs improving API_lab6.1/src/index.js
+++ HTML CSS JavaScript/Lab6_API/use ejs improving API_lab6.1/src/index.js
@@ -8,23 +8,10 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-// чи можне це переставити в папку for devs???
 app.set("view engine", "ejs");
 app.use(express.static('./public'));
 app.use("/images", express.static('./files/img'));
 app.use("/screen", express.static('./files/screenshots_API'));
-//--------------------------------------------------------------
-
-// app.get("/technical-information", (req, res) => {
-//     res.render("Information_and_API",{
-
-//     });
-// });
-// app.get("/author-information", (req, res) => {
-//     res.render("author_Info",{
-
-//     });
-// });
 
 app.use("/for-developers", devRouter)
 app.use("/cars", carRouter);
